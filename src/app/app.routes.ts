2b9eb633277f8fbd38c991998fc06812b2c66d81
Mario Vazquez <mariovazquezbernia@gmail.com>
2026-02-13 17:07:23 +0100

    Primer commit - proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Inicio } from '../layout/inicio/inicio';
+import { Bio } from '../components/bio/bio';
+import { Libros } from '../components/libros/libros';
+import { Productos } from '../components/productos/productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Inicio  },
+    {path: "inicio", component: Inicio  },
+    {path: "sobre-mi", component: Bio},
+    {path: "libros", component: Libros},
+    {path: "productos", component: Productos},
+    {path: "**", redirectTo: "inicio" }
+];
---
+++ src/components/libros/libros.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libros',
+  imports: [],
+  templateUrl: './libros.html',
+  styleUrl: './libros.scss',
+})
+export class Libros {
+
+}
